d1f321976f13487f4cedf099c2a3b9b66a837fc1
echolimitless <echolimitless@outlook.com>
2021-07-05 00:29:55 +0900

    ルーティング完了

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessageComponent } from './message/message.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessageComponent,
+    DashboardComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

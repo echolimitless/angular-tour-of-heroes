dc04178d84e001bd47c8179d74a2baf71fd3b2eb
echolimitless <echolimitless@outlook.com>
2021-07-06 23:28:49 +0900

    ヒーロー削除完了

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,7 +13,7 @@ export class HeroService {
   private heroesUrl = 'api/heroes';
 
   httpOptions = {
-    Headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
   constructor(
@@ -40,6 +40,22 @@ export class HeroService {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
       tap((_) => this.log(`update hero id=${hero.id}`)),
       catchError(this.handleError<any>(`updateHero`))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap((_) => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
     );
   }
 
